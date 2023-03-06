36dea97629882c9aede374e19a234511662aae8b
Zobeir Hamid <zobeirhamid@berkeley.edu>
2023-03-06 09:52:26 -0800

    feat: add @zerodev/wagmi

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -4,8 +4,19 @@ import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { 
+  SocialWalletConnector,
+  GoogleSocialWalletConnector, 
+  FacebookSocialWalletConnector, 
+  GithubSocialWalletConnector,
+  DiscordSocialWalletConnector,
+  TwitchSocialWalletConnector,
+  TwitterSocialWalletConnector,
+} from '@zerodevapp/wagmi'
 
 import { publicProvider } from 'wagmi/providers/public'
+
+const options = { options: { projectId: 'b5486fa4-e3d9-450b-8428-646e757c10f6' } } 
 
 const { chains, provider, webSocketProvider } = configureChains(
   [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
@@ -17,6 +28,12 @@ const { chains, provider, webSocketProvider } = configureChains(
 export const client = createClient({
   autoConnect: true,
   connectors: [
+    new GoogleSocialWalletConnector(options),
+    new FacebookSocialWalletConnector(options),
+    new GithubSocialWalletConnector(options),
+    new DiscordSocialWalletConnector(options),
+    new TwitchSocialWalletConnector(options),
+    new TwitterSocialWalletConnector(options),
     new MetaMaskConnector({ chains }),
     new CoinbaseWalletConnector({
       chains,
